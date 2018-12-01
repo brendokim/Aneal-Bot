fe4c809e5fa12ba90331f6e32456a947723600cc
Jim Wei <33367878+jimwei0620@users.noreply.github.com>
2018-12-01 15:34:48 -0800

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+
+using Discord;
+using Discord.Commands;
+
+namespace anealBot
+{
+    public class HelloWorld: ModuleBase<SocketCommandContext>
+    {
+        [Command("Hello"), Alias("helloworld", "world"), Summary("Hello world command")]
+        public async Task Aneal()
+        {
+            await Context.Channel.SendMessageAsync("Hello world");
+        }
+
+        [Command("Bomb"), Alias("Bombworld", "world"), Summary("Bomb world command")]
+        public async Task Bomb()
+        {
+            await Context.Channel.SendMessageAsync("BOOM");
+        }
+
+
+    }
+}
