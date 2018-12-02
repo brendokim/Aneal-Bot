68d23bc3fb006eed3d4379a78be332f131ca5fe7
brendokim <44178265+brendokim@users.noreply.github.com>
2018-12-01 19:35:57 -0800

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -8,10 +8,9 @@ using Discord.Commands;
 
 namespace anealBot
 {
-    public class HelloWorld: ModuleBase<SocketCommandContext>
+    public class HelloWorld : ModuleBase<SocketCommandContext>
     {
         [Command("Hello"), Alias("helloworld", "world"), Summary("Hello world command")]
-
         public async Task Aneal()
         {
             await Context.Channel.SendMessageAsync("Hello world");
@@ -23,7 +22,27 @@ namespace anealBot
             await Context.Channel.SendMessageAsync("BOOM");
         }
 
+        [Command("Chief"), Alias("Chiefworld", "world"), Summary("Chief world command")]
+        public async Task Chief()
+        {
+            await Context.Channel.SendMessageAsync("Hey Chief");
+        }
 
-       
+        [Command("embed"), Summary("Embed test command")]
+        public async Task Embed([Remainder]string Input = "None")
+        {
+            EmbedBuilder Embed = new EmbedBuilder();
+            Embed.WithAuthor("Test embed", Context.User.GetAvatarUrl());
+            Embed.WithColor(40, 200, 150);
+            Embed.WithFooter("The footer of the embed", Context.Guild.Owner.GetAvatarUrl());
+            Embed.WithDescription("This is a **dummy**   description, with a cool link.\n" +
+                              "[This is my favourite website](https://niceme.me/)");
+            Embed.AddInlineField("User input:", Input); 
+
+
+            await Context.Channel.SendMessageAsync("", false, Embed.Build());
+        }
     }
+
 }
+
